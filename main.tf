6c5d07fd1c31fc2d0446371b74b7e7e8f03365ee
ashish <get.mail.ashish@gmail.com>
2022-03-28 11:50:07 +0530

    [successful code deployment]

--- main.tf
+++ main.tf
@@ -139,12 +139,6 @@ resource "aws_key_pair" "terraform-key" {
   public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQC4on3GTiWZsBS5XZCrottdyFZRnmBofdCkTvZDEx5p36NCHM6ba969XLMzoHKv+ArNqwhqQDxiTZ8gXAGcK0EcZ+uAJBCQaOmfl9qX+AWBJeC2d0Zbainw2ShLFxpXOyGQcc9MaiJAW0d+ADqvXiLkdtNdtWBRCOiv0YPa2aoq8FCLL9CaIw2u6sM+QlsKiwppRPiCJAslB5B+1Ti+x9Ce5nBZfQfcoZ+Cuf3cIRQv5GOijPoXENPO7+4N514nV7FCbJZq36sgVXgQGtmnDttj96UiBziPhfLAbzz6LvkKAbmuu4GG+C2RCBfv0SOs2Pkhf/ffWEExkYZbYmREW9TUybzn3XDSDfQROCBdlDUySr0ge/xbEOY2S3CcyKsV6221/Dc6CCyXRCJdueopIXoNfD+J0Koi0gMYzMwmHMusEkjdBzeqQhrw26ND8eBEZhUlzlOCTFutTqhgaJvKZ1G4c1BAEPEthwRE2KrtDh4KsI9rx08IulEpdwwmpHENuBk= getma@DESKTOP-59V52CL"
 }
 
-data "aws_instance" "private-instance" {
-  filter {
-    name   = "tag:Name"
-    values = ["terraform-instance-25-03-2022-[private]"]
-  }
-}
 
 
 resource "aws_instance" "terraform-instance" {
@@ -165,10 +159,7 @@ resource "aws_instance" "terraform-instance" {
     source      = "credentials/k3s_ssh"
     destination = "/home/ec2-user/private_key"
   }
-  provisioner "file" {
-    source      = "node-token"
-    destination = "/home/ec2-user/node-token"
-  }
+
   provisioner "file" {
     source      = "worker_script.sh"
     destination = "/home/ec2-user/worker_script.sh"
@@ -178,10 +169,13 @@ resource "aws_instance" "terraform-instance" {
     inline = [
       "sudo chmod +x /home/ec2-user/script.sh",
       "sudo sh /home/ec2-user/script.sh",
-      "sudo cp /home/ec2-user/node-token /var/lib/rancher/k3s/server/node-token",
       "sudo chmod +x /home/ec2-user/worker_script.sh",
       "sudo chown ec2-user:root /home/ec2-user/worker_script.sh",
-      "sudo scp -i private_key worker_script.sh node-token ec2-user@${data.aws_instance.private-instance.private_ip}:/home/ec2-user/"
+      "sudo chmod 400 /home/ec2-user/private_key",
+      "sudo /home/ec2-user/worker_script.sh > /home/ec2-user/worker.sh",
+      "sudo chmod +x /home/ec2-user/worker.sh",
+      "sudo scp -o StrictHostKeyChecking=no -i private_key /home/ec2-user/worker.sh ec2-user@${aws_instance.terraform-instance-private.private_ip}:/home/ec2-user/",
+      "sudo ssh -o StrictHostKeyChecking=no -i private_key  ec2-user@${aws_instance.terraform-instance-private.private_ip} '/home/ec2-user/worker.sh' "
     ]
   }
   connection {
@@ -216,12 +210,6 @@ resource "aws_security_group" "terraform-sg-private" {
   }
 }
 
-data "aws_instance" "public-instance" {
-  filter {
-    name   = "tag:Name"
-    values = ["terraform-instance-25-03-2022"]
-  }
-}
 
 resource "aws_instance" "terraform-instance-private" {
   ami             = var.ami
@@ -232,13 +220,11 @@ resource "aws_instance" "terraform-instance-private" {
   tags = {
     Name = "terraform-instance-25-03-2022-[private]"
   }
-
 }
 
-
-
-
-
+output "private-ip" {
+  value = aws_instance.terraform-instance-private.private_ip
+}
 
 
 
